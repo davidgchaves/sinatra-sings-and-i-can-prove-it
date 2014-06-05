d1b5fa071fd7f0590ca446b4c8c69eb5fad67450
David G Chaves <davidgchaves@gmail.com>
2014-06-05 09:24:44 +0200

    Sinatra would like to sing the Capybara_meets_Cucumber tune

---
+++ features/step_definitions/tune_steps.rb
@@ -0,0 +1,7 @@
+When /^Sinatra is told to sing the (\w+) tune$/ do |tune_title|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then /^he sings the (\w+) tune loud and clear$/ do |tune_title|
+    pending # express the regexp above with the code you wish you had
+end
---
+++ features/the_cucumber_tune.feature
@@ -0,0 +1,5 @@
+Feature: The Cucumber Tune
+
+  Scenario: Sinatra sings and I can prove it
+    When Sinatra is told to sing the Capybara_meets_Cucumber tune
+    Then he sings the Capybara_meets_Cucumber tune loud and clear
